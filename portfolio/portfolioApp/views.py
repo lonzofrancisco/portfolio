c1bb918a0cc5c2ac122edf88e6b7c7db9014e25a
lonzofrancisco <lonzofrancisco@gmail.com>
2022-01-26 23:27:24 -0300

    add sendmail logic

--- portfolio/portfolioApp/views.py
+++ portfolio/portfolioApp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, HttpResponse
+from django.core.mail import send_mail
+from portfolio import settings
 # Create your views here.
 
 
@@ -6,11 +8,33 @@ from django.shortcuts import render, HttpResponse
 def home(request):
     return render(request, 'portfolio/home.html')
 
-def contact(request):
-    return render(request, 'portfolio/contact.html')
 
 def aboutme(request):
     return render(request, 'portfolio/about-me.html')
 
 def projects(request):
     return render(request, 'portfolio/projects.html')
+
+def contact(request):
+
+    if request.method == 'POST':
+        print("posteo")
+
+        subject = request.POST['Nombre'] + ' ' +request.POST['apellido']
+        message = request.POST['msg']           
+        mail= request.POST['emailInfo']  
+        phone= request.POST['phoneNumber']  
+
+        #asunto = nombre + " " + apellido
+        #contenido =  email + " " + phoneNumber + " " + msg
+        email_from= settings.EMAIL_HOST_USER
+        recipient_list=['lonzofrancisco@gmail.com']
+        header = "MAIL DESDE PORTAFOLIO!!" + ' '+ mail
+        message = "Hola soy: " + subject +  ' Mi numero es: ' + phone + "\n" + 'Mensaje: '+ message
+        send_mail(header,message,email_from,recipient_list)
+
+
+
+
+
+    return render(request, 'portfolio/contact.html')
